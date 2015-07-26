b412a09618845fed27cf624a45b10776736f495b
Ashish Patil <ashishnitinpatil@gmail.com>
2015-07-26 21:41:18 +0530

    +public history continuous fetch script
    Given the fetch delay, it pulls all the latest matches from
    start_at_match_seq_num till number of matches_requested are fetched or
    any error occurs

---
+++ scripts/public_history.py
@@ -0,0 +1,55 @@
+"""
+Script to fetch history for the latest matches (global, public)
+"""
+
+from __future__ import print_function
+from dota2py import api
+from time import sleep as wait_for_next_fetch
+
+def public_match_history(start_at_match_seq_num=None, fetch_delay=1,
+                         matches_requested=10**5,
+                         **kwargs):
+    """
+    Returns list of most recent public matches according to given kwargs
+    Rate limits the API requests according to `fetch_delay` (in seconds)
+    Output : last_response_status, last_response_detail, match_history
+    """
+
+    # tracking variables
+    matches_fetched = 0
+    last_match_id = None
+    last_response_status = 1
+    match_history = []
+    last_response_detail = "Fetch successful"
+
+    while last_response_status == 1 and matches_fetched < matches_requested:
+        cur_response = api.get_match_history_by_sequence_num(
+            start_at_match_seq_num=last_match_id, **kwargs
+        )
+        last_response_status = cur_response['result']['status']
+        if not last_response_status == 1:
+            # unsuccessful query
+            if not 'statusDetail' in cur_response['result']:
+                last_response_detail = "Unknown error"
+            else:
+                last_response_detail = cur_response['result']['statusDetail']
+            break
+        else:
+            # successful data fetch
+            if len(cur_response['result']['matches']) > 1:
+                if not match_history:
+                    # very first fetch
+                    match_history.extend(cur_response['result']['matches'])
+                    matches_fetched += len(cur_response['result']['matches'])
+                else:
+                    # 2nd fetch onwards, ignore the first common match
+                    match_history.extend(cur_response['result']['matches'][1:])
+                    matches_fetched += len(cur_response['result']['matches']) - 1
+            else:
+                break
+            last_match_id = cur_response['result']['matches'][-1]['match_id']
+        print("Matches fetched - #{}...".format(matches_fetched))
+        wait_for_next_fetch(fetch_delay)
+    print("{0}: {1}".format(last_response_status, last_response_detail))
+    return {'status':last_response_status, 'statusDetail':last_response_detail,
+            'matches':match_history}
