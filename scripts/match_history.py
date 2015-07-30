044e6674f2d0997cd0cbe63e54e1b16759f91b31
Ashish Patil <ashishnitinpatil@gmail.com>
2015-07-30 22:27:35 +0530

    ~non-dependence on correct response
    num_results is returned as 0 for league matches even if positive number
    of matches are returned
    DRY for response dict

--- scripts/match_history.py
+++ scripts/match_history.py
@@ -35,18 +35,21 @@ def latest_matches(account_id=None, start_at_match_id=None,
             break
         else:
             # successful data fetch
-            if cur_response['result']['num_results'] > 1:
+            cur_matches = cur_response['result']['matches']
+            if len(cur_matches) >= 1:
                 if not match_history:
                     # very first fetch
-                    match_history.extend(cur_response['result']['matches'])
-                    matches_fetched += cur_response['result']['num_results']
+                    match_history.extend(cur_matches)
+                    matches_fetched += len(cur_matches)
                 else:
                     # 2nd fetch onwards, ignore the first common match
-                    match_history.extend(cur_response['result']['matches'][1:])
-                    matches_fetched += cur_response['result']['num_results'] - 1
+                    match_history.extend(cur_matches[1:])
+                    matches_fetched += len(cur_matches) - 1
+                if len(cur_matches) == 1 and cur_matches[0]['match_id'] == last_match_id:
+                    break
             else:
                 break
-            last_match_id = cur_response['result']['matches'][-1]['match_id']
+            last_match_id = cur_matches[-1]['match_id']
         if debug:
             print("Matches fetched - #{}...".format(matches_fetched))
         wait_for_next_fetch(fetch_delay)
