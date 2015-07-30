228c1c88619a76327e4577f84993dbf64521538f
Ashish Patil <ashishnitinpatil@gmail.com>
2015-07-30 22:31:06 +0530

    ~better code readability (DRY for dict operations)
    Also added sense check if response contains only 1 match data

--- scripts/historical_data.py
+++ scripts/historical_data.py
@@ -36,18 +36,21 @@ def public_match_history(start_at_match_seq_num=None, matches_requested=500,
             break
         else:
             # successful data fetch
-            if len(cur_response['result']['matches']) > 1:
+            cur_matches = cur_response['result']['matches']
+            if len(cur_response['result']['matches']) >= 1:
                 if not match_history:
                     # very first fetch
-                    match_history.extend(cur_response['result']['matches'])
-                    matches_fetched += len(cur_response['result']['matches'])
+                    match_history.extend(cur_matches)
+                    matches_fetched += len(cur_matches)
                 else:
                     # 2nd fetch onwards, ignore the first common match
-                    match_history.extend(cur_response['result']['matches'][1:])
-                    matches_fetched += len(cur_response['result']['matches']) - 1
+                    match_history.extend(cur_matches[1:])
+                    matches_fetched += len(cur_matches) - 1
+                if len(cur_matches) == 1 and cur_matches[0]['match_id'] == last_match_id:
+                    break
             else:
                 break
-            last_match_seq_num = cur_response['result']['matches'][-1]['match_seq_num']
+            last_match_seq_num = cur_matches[-1]['match_seq_num']
         if debug:
             print("Matches fetched - #{}...".format(matches_fetched))
         wait_for_next_fetch(fetch_delay)
