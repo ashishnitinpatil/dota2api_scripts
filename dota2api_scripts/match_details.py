e3026bd666a66892365f7a0b5200d56b7852de18
Ashish Patil <ashishnitinpatil@gmail.com>
2015-08-02 03:44:31 +0530

    ~Bumping fetch_delay for match_details
    Ran into numerous ConnectionErrors when trying to fetch TI5 matches

--- dota2api_scripts/match_details.py
+++ dota2api_scripts/match_details.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 from dota2py import api
 from time import sleep as wait_for_next_fetch
 
-def match_details(match_ids=[], fetch_delay=0.2, debug=True, **kwargs):
+def match_details(match_ids=[], fetch_delay=2, debug=True, **kwargs):
     """
     Returns data for the requested match ids
     Rate limits the API requests according to `fetch_delay` (in seconds)
@@ -17,17 +17,19 @@ def match_details(match_ids=[], fetch_delay=0.2, debug=True, **kwargs):
     times_failed = 0
 
     for i, match_id in enumerate(match_ids):
+        if debug:
+            print(i, "Trying match id", match_id)
         cur_response = api.get_match_details(match_id=match_id, **kwargs)
         if not 'match_id' in cur_response['result']:
             # unsuccessful query
             times_failed += 1
             if debug:
-                print(i, "Match id", match_id, ":", "Unsuccessful fetch / bad id")
+                print("Match id", match_id, ":", "Unsuccessful fetch / bad id")
         else:
             # successful data fetch
             all_details[match_id] = cur_response['result']
             if debug:
-                print(i, "Match id", match_id, ":", "Successful fetch")
+                print("Match id", match_id, ":", "Successful fetch")
         wait_for_next_fetch(fetch_delay)
     if debug:
         print("Matches fetched = #", len(all_details), sep="")
